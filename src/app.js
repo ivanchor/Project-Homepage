cac7b81291acb4a31a60790b7c41c68e2a42a363
ivanchor <ichor85@gmail.com>
2026-06-25 17:58:17 -0700

    Placeholder project showcase section

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import './global.css'
 import {createHeader} from './header/header.js'
-
+import { createProject } from './project/project.js'
 
 
 export function initializePage(){
     document.getElementById('header-container').innerHTML = createHeader()
+    document.getElementById('project-container').innerHTML = createProject()
 }
 
