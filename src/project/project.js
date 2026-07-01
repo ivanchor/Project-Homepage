5ee63a02e18067a3292f1185272a1d9d77d110dd
ivanchor <ichor85@gmail.com>
2026-06-30 22:36:03 -0700

    Small changes to project section

--- src/project/project.js
+++ src/project/project.js
@@ -25,8 +25,7 @@ export function createProject(){
 function createProjectShowcase(projectLink, githubLink, image, title, description){
     return`
         <div class='project-showcase'>
-                <a 
-                    href='${projectLink}'>
+                <a href='${projectLink}'>
                     <img class='project-showcase-image'
                         src='${image}'
                         alt='Project image'
@@ -44,11 +43,14 @@ function createProjectShowcase(projectLink, githubLink, image, title, descriptio
                                     src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" 
                                     alt='Github'/>
                             </a>
+                            <a href='${projectLink}'>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg>
+                            </a>
                         </div>
                     </h3>
 
                     <p class='project-showcase-body'>${description}</p>
                 </div>                
-            </div>
+        </div>
     `
 }
