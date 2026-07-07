631b16ed9336e10baa2bc1d23ecffaaf4ad87ecc
ivanchor <ichor85@gmail.com>
2026-07-07 14:57:36 -0700

    Added content to contact section

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,6 +1,36 @@
+import './contact.css'
+import contactImage from '.././assets/photos/contactImage.PNG'
+import test from '.././assets/photos/project1.jpg'
+
 export function createContact(){
     return `
-        <footer>
+        <footer class='contact-main'>
+            <div class='contact-maincontainer'>
+                <div class='contact-content'>
+                    <h1>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>star</title><path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" /></svg>
+                        Contact Info
+                    </h1>
+
+                    <p>Please contact me through these means if you like my work.</p>
+
+                    <p class='contact-info'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>phone</title><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" /></svg>
+                        Phone Number
+                    </p>
+
+                    <p class='contact-info'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>email</title><path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
+                        Email Address
+                    </p>
+                </div>
+
+                <img class='contact-image' 
+                    src='${test}'
+                    alt='Contact-Image'
+                >
+
+            </div>
         </footer> 
     `
 }
